a6fc4526a4dabb1efcb042781281fca69a0afbf2
Davidm8624 <davidm8624@gmail.com>
2023-05-10 15:36:01 -0700

    basic setup

--- game.rb
+++ game.rb
@@ -29,14 +29,78 @@
 #shorthand way to do it:
 
 class Dungeon
-  attr_acessor :player
+  attr_accessor :player
 
   def initialize(player)
     @player = player
-
     @rooms = {}
+  end
+
+  def add_room(reference, name, description, connections)
+    @rooms[reference] = Room.new(reference, name, description, connections)
+  end
+
+  def start (locatiion)
+    @player.location = location
+    show_current_description
+  end
+
+  def show_current_description
+    puts find_room_in_dungeon(@player_location).full_description
+  end
+
+  def find_room_in_dungeon(reference)
+    @rooms[reference]
+  end
+
+  def find_room_in_direction(direction)
+    find_room_in_dungeon(@player,location).connections[direction]
+  end
+
+  def go(direction)
+    puts "You go " + direction.to_s
+    @player.location = find_room_in_direction(direction)
+    show_current_description
   end
 end
 
-Player = Struct.new(:name, :location)
-Room = Struct.new(:reference, :name, :decription, :connections)
+class Player
+  attr_accessor :name, :location
+  def initialize(name)
+    @name = name
+  end
+end
+
+class Room
+  attr_accessor :reference, :name, :description, :connections
+  def initialize(reference, name, description, connections)
+    @reference = reference
+    @name = name
+    @description = description
+    @connections = connections
+  end
+
+  def full_description
+    @name + "\n\nYou are in" + @decription
+  end
+end
+
+player = Player.new("First Last")
+my_dungeon = Dungeon.new(player)
+
+my_dungeon.add_room(:largecave,
+"Large cave",
+"a large cavernous cave",
+{west: :smallcave})
+
+my_dungeon.add_room(:smallcave,
+"Small Cave",
+"a small cave",
+{east: :largecave})
+
+#starts by placing player in the large cave
+
+_my_dungeon.start(:largecave)
+
+end
+

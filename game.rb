9cf05bd18ff7e4cbef81ed5acaf21c1c7d7125ce
Davidm8624 <davidm8624@gmail.com>
2023-05-10 15:12:37 -0700

    shorthand

--- game.rb
+++ game.rb
@@ -1,27 +1,42 @@
 # 3 classes:
 
+# class Dungeon
+#   arrt_acessor :player
+
+#   def initialize(player)
+#     @player = player
+#     @rooms = {}
+#   end
+# end
+
+# class Player
+#   attr_acessor :name, :location
+#   def initialize(player_name)
+#     @name = player_name
+#   end
+# end
+
+# class Room
+#   attr_acessor :reference, :name, :decription, :connections
+
+#   def initialize (reference, name, description,connections)
+#     @reference = reference
+#     @description = description
+#     @connections = connections
+#   end
+# end
+
+#shorthand way to do it:
+
 class Dungeon
-  arrt_acessor :player
+  attr_acessor :player
 
   def initialize(player)
     @player = player
+
     @rooms = {}
   end
 end
 
-class Player
-  attr_acessor :name, :location
-  def initialize(player_name)
-    @name = player_name
-  end
-end
-
-class Room
-  attr_acessor :reference, :name, :decription, :connections
-
-  def initialize (reference, name, description,connections)
-    @reference = reference
-    @description = description
-    @connections = connections
-  end
-end
+Player = Struct.new(:name, :location)
+Room = Struct.new(:reference, :name, :decription, :connections)
